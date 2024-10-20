9467810b8d8488891a6cd6ebc12d0fff5bde94de
Harvey118 <feats28@gmail.com>
2024-10-20 16:38:25 +0800

    第一次提交

--- DAY1/main.js
+++ DAY1/main.js
@@ -1,6 +1,6 @@
 let a = 100
 let b = 100
-console.error('HELLo', a+b)
-console.warn('HELLo', a+b)
-console.log('HELLo', a+b)
+console.error('HELLo', a + b)
+console.warn('HELLo', a + b)
+console.log('HELLo', a + b)
 console.log(`HELLo ${a} ${b}`)
